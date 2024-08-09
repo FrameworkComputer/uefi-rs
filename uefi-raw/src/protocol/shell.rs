25dff5f9903f6a107d193d7e8944e5ce573c4d72
Daniel Schaefer <dhs@frame.work>
2024-08-09 17:49:52 +0800

    clean up shell protocol
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

---
+++ uefi-raw/src/protocol/shell.rs
@@ -0,0 +1,190 @@
+use core::{ffi::c_void, ptr::NonNull};
+
+use crate::{guid, Guid, Char16, Event, Handle, Status};
+use crate::time::Time;
+use crate::protocol::file_system::FileAttribute;
+
+#[repr(u64)]
+#[derive(Debug)]
+/// Open modes
+///
+/// Can be added together to combine them.
+pub enum FileOpenMode {
+    /// Open file for reading
+    Read = 0x0000000000000001,
+    /// Open file for writing
+    Write = 0x0000000000000002,
+    /// Create the file if it doesn't exist already
+    Create = 0x8000000000000000,
+}
+
+/// TODO
+#[derive(Debug)]
+#[repr(C)]
+pub struct ShellProtocol {
+    pub execute: extern "efiapi" fn(
+        parent_image_handle: *const Handle,
+        commandline: *const Char16,
+        environment: *const *const Char16,
+        out_status: *mut Status,
+    ) -> Status,
+    pub get_env: usize,
+    pub set_env: usize,
+    pub get_alias: usize,
+    pub set_alias: usize,
+    pub get_help_text: usize,
+    pub get_device_path_from_map: usize,
+    pub get_map_from_device_path: usize,
+    pub get_device_path_from_file_path: usize,
+    pub get_file_path_from_device_path: usize,
+    pub set_map: usize,
+
+    pub get_cur_dir: usize,
+    pub set_cur_dir: usize,
+    pub open_file_list: usize,
+    pub free_file_list: usize,
+    pub remove_dup_in_file_list: usize,
+
+    pub batch_is_active: extern "efiapi" fn() -> bool,
+    pub is_root_shell: usize,
+    pub enable_page_break: extern "efiapi" fn(),
+    pub disable_page_break: extern "efiapi" fn(),
+    pub get_page_break: usize,
+    pub get_device_name: usize,
+
+    pub get_file_info: extern "efiapi" fn(file_handle: ShellFileHandle) -> *const FileInfo,
+    pub set_file_info: extern "efiapi" fn(file_handle: ShellFileHandle, file_info: &FileInfo) -> Status,
+    pub open_file_by_name: extern "efiapi" fn(
+        path: *const u16,
+        file_handle: *mut ShellFileHandle,
+        open_mode: u64,
+    ) -> Status,
+    pub close_file: extern "efiapi" fn(file_handle: ShellFileHandle) -> Status,
+    pub create_file: extern "efiapi" fn(
+        file_name: *const Char16,
+        file_attribs: u64,
+        out_file_handle: *mut ShellFileHandle,
+    ) -> Status,
+    pub read_file: extern "efiapi" fn(
+        file_handle: ShellFileHandle,
+        read_size: &mut usize,
+        buffer: *mut c_void,
+    ) -> Status,
+    pub write_file: extern "efiapi" fn(
+        file_handle: ShellFileHandle,
+        buffer: &mut usize,
+        buffer: *const c_void,
+    ) -> Status,
+    pub delete_file: extern "efiapi" fn(file_handle: ShellFileHandle) -> Status,
+    pub delete_file_by_name: extern "efiapi" fn(file_name: *const Char16) -> Status,
+    pub get_file_position: usize,
+    pub set_file_position: usize,
+    pub flush_file: usize,
+    pub find_files: extern "efiapi" fn(
+        file_pattern: *const Char16,
+        out_file_list: *mut *mut ShellFileInfo,
+    ) -> Status,
+    pub find_files_in_dir: usize,
+    pub get_file_size: extern "efiapi" fn(file_handle: ShellFileHandle, size: *mut u64) -> Status,
+
+    pub open_root: usize,
+    pub open_root_by_handle: usize,
+
+    /// Event to check if the user has requested execution to be stopped (CTRL-C)
+    pub execution_break: Event,
+
+    /// Major version of the shell
+    pub major_version: u32,
+    /// Minor version of the shell
+    pub minor_version: u32,
+    pub register_guid_name: usize,
+    pub get_guid_name: usize,
+    pub get_guid_from_name: usize,
+    pub get_env_ex: usize,
+}
+
+impl ShellProtocol {
+    pub const GUID: Guid = guid!("6302d008-7f9b-4f30-87ac-60c9fef5da4e");
+}
+
+/// TODO
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+pub struct ShellFileHandle(NonNull<c_void>);
+
+/// TODO
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct ShellFileInfo {
+    link: ListEntry,
+    status: Status,
+    full_name: *const Char16,
+    file_name: *const Char16,
+    shell_file_handle: Handle,
+    info: *mut FileInfo,
+}
+
+impl ShellFileInfo {
+    pub fn from_list_entry_ptr(entry: *const ListEntry) -> Self {
+        // Safety: This is safe due to the C representation of the two structs;
+        // Every [`ShellFileInfo`] starts with a [`ListEntry`] so a pointer to [`ListEntry`]
+        // is a pointer to [`ShellFileInfo`] as well.
+        unsafe { *entry.cast::<ShellFileInfo>() }
+    }
+}
+
+/// TODO
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct ListEntry {
+    flink: *mut ListEntry,
+    blink: *mut ListEntry,
+}
+
+// TODO: Already defined in uefi/src/proto/media/file/info.rs
+use ptr_meta::Pointee;
+#[derive(Debug, Eq, PartialEq, Pointee)]
+#[repr(C)]
+pub struct FileInfo {
+    size: u64,
+    file_size: u64,
+    physical_size: u64,
+    create_time: Time,
+    last_access_time: Time,
+    modification_time: Time,
+    attribute: FileAttribute,
+    file_name: [Char16],
+}
+
+/// TODO
+#[derive(Debug)]
+pub struct ShellFileIter {
+    current_node: *const ListEntry,
+}
+
+impl ShellFileIter {
+    pub fn from_file_info_ptr(ptr: *const ShellFileInfo) -> Self {
+        // Safety: This is safe, as each [`ShellFileInfo`] begins
+        // with a [`ListEntry`] and are #[repr(C)] structs
+        Self {
+            current_node: ptr.cast::<ListEntry>(),
+        }
+    }
+}
+
+impl Iterator for ShellFileIter {
+    type Item = ShellFileInfo;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Safety: This is safe as we're dereferencing a pointer that we've already null-checked
+        unsafe {
+            if (*self.current_node).flink.is_null() {
+                None
+            } else {
+                let ret = ShellFileInfo::from_list_entry_ptr((*self.current_node).flink);
+                self.current_node = (*self.current_node).flink;
+                Some(ret)
+            }
+        }
+    }
+}
--- uefi/src/proto/shell/mod.rs
+++ uefi/src/proto/shell/mod.rs
@@ -1,111 +1,16 @@
 //! EFI Shell Protocol v2.2
 
-use core::{ffi::c_void, mem::MaybeUninit, ptr::NonNull};
+use core::{ffi::c_void, mem::MaybeUninit};
 
 use crate::proto::unsafe_protocol;
-use crate::{CStr16, Event, Handle, Result, Status, StatusExt};
+use crate::{CStr16, Handle, Result, Status, StatusExt};
+use uefi_raw::protocol::shell::*;
 
-use super::media::file::FileInfo;
-
-#[repr(u64)]
+/// The Shell protocol.
 #[derive(Debug)]
-/// Open modes
-///
-/// Can be added together to combine them.
-pub enum FileOpenMode {
-    /// Open file for reading
-    Read = 0x0000000000000001,
-    /// Open file for writing
-    Write = 0x0000000000000002,
-    /// Create the file if it doesn't exist already
-    Create = 0x8000000000000000,
-}
-
-/// TODO
-#[repr(C)]
-#[derive(Debug)]
-#[unsafe_protocol("6302d008-7f9b-4f30-87ac-60c9fef5da4e")]
-pub struct Shell {
-    execute: extern "efiapi" fn(
-        parent_image_handle: *const Handle,
-        commandline: *const CStr16,
-        environment: *const *const CStr16,
-        out_status: *mut Status,
-    ) -> Status,
-    get_env: usize,
-    set_env: usize,
-    get_alias: usize,
-    set_alias: usize,
-    get_help_text: usize,
-    get_device_path_from_map: usize,
-    get_map_from_device_path: usize,
-    get_device_path_from_file_path: usize,
-    get_file_path_from_device_path: usize,
-    set_map: usize,
-
-    get_cur_dir: usize,
-    set_cur_dir: usize,
-    open_file_list: usize,
-    free_file_list: usize,
-    remove_dup_in_file_list: usize,
-
-    batch_is_active: extern "efiapi" fn() -> bool,
-    is_root_shell: usize,
-    enable_page_break: extern "efiapi" fn(),
-    disable_page_break: extern "efiapi" fn(),
-    get_page_break: usize,
-    get_device_name: usize,
-
-    get_file_info: extern "efiapi" fn(file_handle: ShellFileHandle) -> *const FileInfo,
-    set_file_info: extern "efiapi" fn(file_handle: ShellFileHandle, file_info: &FileInfo) -> Status,
-    open_file_by_name: extern "efiapi" fn(
-        path: *const u16,
-        file_handle: *mut ShellFileHandle,
-        open_mode: u64,
-    ) -> Status,
-    close_file: extern "efiapi" fn(file_handle: ShellFileHandle) -> Status,
-    create_file: extern "efiapi" fn(
-        file_name: &CStr16,
-        file_attribs: u64,
-        out_file_handle: *mut ShellFileHandle,
-    ) -> Status,
-    read_file: extern "efiapi" fn(
-        file_handle: ShellFileHandle,
-        read_size: &mut usize,
-        buffer: *mut c_void,
-    ) -> Status,
-    write_file: extern "efiapi" fn(
-        file_handle: ShellFileHandle,
-        buffer: &mut usize,
-        buffer: *const c_void,
-    ) -> Status,
-    delete_file: extern "efiapi" fn(file_handle: ShellFileHandle) -> Status,
-    delete_file_by_name: extern "efiapi" fn(file_name: &CStr16) -> Status,
-    get_file_position: usize,
-    set_file_position: usize,
-    flush_file: usize,
-    find_files: extern "efiapi" fn(
-        file_pattern: *const CStr16,
-        out_file_list: *mut *mut ShellFileInfo,
-    ) -> Status,
-    find_files_in_dir: usize,
-    get_file_size: extern "efiapi" fn(file_handle: ShellFileHandle, size: *mut u64) -> Status,
-
-    open_root: usize,
-    open_root_by_handle: usize,
-
-    /// Event to check if the user has requested execution to be stopped (CTRL-C)
-    pub execution_break: Event,
-
-    /// Major version of the shell
-    pub major_version: u32,
-    /// Minor version of the shell
-    pub minor_version: u32,
-    register_guid_name: usize,
-    get_guid_name: usize,
-    get_guid_from_name: usize,
-    get_env_ex: usize,
-}
+#[repr(transparent)]
+#[unsafe_protocol(ShellProtocol::GUID)]
+pub struct Shell(ShellProtocol);
 
 impl Shell {
     /// TODO
@@ -115,43 +20,43 @@ impl Shell {
         command_line: &CStr16,
         environment: &[&CStr16],
     ) -> Result {
+        let mut out_status: MaybeUninit<Status> = MaybeUninit::uninit();
         // This is required because the compiler won't turn `&[&Cstr16]`
         // or `*const &Cstr16 into *const *const CStr16`
-        let mut out_status: MaybeUninit<Status> = MaybeUninit::uninit();
         let environment = environment.as_ptr();
-        let environment = environment.cast::<*const CStr16>();
+        let environment = environment.cast::<*const u16>();
 
-        (self.execute)(
-            &parent_image,
-            command_line,
+        (self.0.execute)(
+            parent_image.as_ptr().cast(),
+            command_line.as_ptr().cast(),
             environment,
             out_status.as_mut_ptr(),
         )
         .to_result()
-        //.into_with_val(|| unsafe { out_status.assume_init() })
+        //.to_result_with_val(|| unsafe { out_status.assume_init() })
     }
 
     /// Returns `true` if any script files are currently being processed.
     #[must_use]
     pub fn batch_is_active(&self) -> bool {
-        (self.batch_is_active)()
+        (self.0.batch_is_active)()
     }
 
     /// Disables the page break output mode.
     pub fn disable_page_break(&self) {
-        (self.disable_page_break)()
+        (self.0.disable_page_break)()
     }
 
     /// Enables the page break output mode.
     pub fn enable_page_break(&self) {
-        (self.enable_page_break)()
+        (self.0.enable_page_break)()
     }
 
     /// Gets the file information from an open file handle
     // TODO: How do we free this automatically?
     // Doesn't seem to work!
     pub fn get_file_info(&self, file_handle: ShellFileHandle) -> Option<&FileInfo> {
-        let info = (self.get_file_info)(file_handle);
+        let info = (self.0.get_file_info)(file_handle);
         if info.is_null() {
             None
         } else {
@@ -161,7 +66,7 @@ impl Shell {
 
     /// Sets the file information to an opened file handle
     pub fn set_file_info(&self, file_handle: ShellFileHandle, file_info: &FileInfo) -> Result {
-        (self.set_file_info)(file_handle, file_info).to_result()
+        (self.0.set_file_info)(file_handle, file_info).to_result()
     }
 
     /// Opens a file or a directory by file name
@@ -171,7 +76,7 @@ impl Shell {
         mode: u64,
     ) -> Result<Option<ShellFileHandle>> {
         let mut out_file_handle: MaybeUninit<Option<ShellFileHandle>> = MaybeUninit::zeroed();
-        (self.open_file_by_name)(
+        (self.0.open_file_by_name)(
             file_name.as_ptr(),
             out_file_handle.as_mut_ptr().cast(),
             mode,
@@ -186,7 +91,7 @@ impl Shell {
     /// Per the UEFI spec, the file handle will be closed in all cases and this function
     /// only returns [`Status::SUCCESS`].
     pub fn close_file(&self, file_handle: ShellFileHandle) -> Result {
-        (self.close_file)(file_handle).to_result()
+        (self.0.close_file)(file_handle).to_result()
     }
 
     /// TODO
@@ -199,7 +104,7 @@ impl Shell {
         // the returned handle can possibly be NULL, so we need to wrap `ShellFileHandle` in an `Option`
         let mut out_file_handle: MaybeUninit<Option<ShellFileHandle>> = MaybeUninit::zeroed();
 
-        (self.create_file)(file_name, file_attribs, out_file_handle.as_mut_ptr().cast())
+        (self.0.create_file)(file_name.as_ptr().cast(), file_attribs, out_file_handle.as_mut_ptr().cast())
             // Safety: if this call is successful, `out_file_handle`
             // will always be initialized and valid.
             .to_result_with_val(|| unsafe { out_file_handle.assume_init() })
@@ -208,7 +113,7 @@ impl Shell {
     /// Reads data from the file
     pub fn read_file(&self, file_handle: ShellFileHandle, buffer: &mut [u8]) -> Result {
         let mut read_size = buffer.len();
-        (self.read_file)(
+        (self.0.read_file)(
             file_handle,
             &mut read_size,
             buffer.as_mut_ptr() as *mut c_void,
@@ -219,7 +124,7 @@ impl Shell {
     /// Writes data to the file
     pub fn write_file(&self, file_handle: ShellFileHandle, buffer: &[u8]) -> Result {
         let mut read_size = buffer.len();
-        (self.write_file)(
+        (self.0.write_file)(
             file_handle,
             &mut read_size,
             buffer.as_ptr() as *const c_void,
@@ -229,18 +134,18 @@ impl Shell {
 
     /// TODO
     pub fn delete_file(&self, file_handle: ShellFileHandle) -> Result {
-        (self.delete_file)(file_handle).to_result()
+        (self.0.delete_file)(file_handle).to_result()
     }
 
     /// TODO
     pub fn delete_file_by_name(&self, file_name: &CStr16) -> Result {
-        (self.delete_file_by_name)(file_name).to_result()
+        (self.0.delete_file_by_name)(file_name.as_ptr().cast()).to_result()
     }
 
     /// TODO
     pub fn find_files(&self, file_pattern: &CStr16) -> Result<Option<ShellFileIter>> {
         let mut out_list: MaybeUninit<*mut ShellFileInfo> = MaybeUninit::uninit();
-        (self.find_files)(file_pattern, out_list.as_mut_ptr()).to_result_with_val(|| {
+        (self.0.find_files)(file_pattern.as_ptr().cast(), out_list.as_mut_ptr()).to_result_with_val(|| {
             unsafe {
                 // safety: this is initialized after this call succeeds, even if it's
                 // null
@@ -258,76 +163,9 @@ impl Shell {
     /// Gets the size of a file
     pub fn get_file_size(&self, file_handle: ShellFileHandle) -> Result<u64> {
         let mut file_size: MaybeUninit<u64> = MaybeUninit::zeroed();
-        (self.get_file_size)(file_handle, file_size.as_mut_ptr().cast())
+        (self.0.get_file_size)(file_handle, file_size.as_mut_ptr().cast())
             // Safety: if this call is successful, `out_file_handle`
             // will always be initialized and valid.
             .to_result_with_val(|| unsafe { file_size.assume_init() })
-    }
-}
-
-/// TODO
-#[repr(transparent)]
-#[derive(Clone, Copy, Debug)]
-pub struct ShellFileHandle(NonNull<c_void>);
-
-/// TODO
-#[repr(C)]
-#[derive(Debug, Clone, Copy)]
-pub struct ShellFileInfo {
-    link: ListEntry,
-    status: Status,
-    full_name: *const CStr16,
-    file_name: *const CStr16,
-    shell_file_handle: Handle,
-    info: *mut FileInfo,
-}
-
-impl ShellFileInfo {
-    fn from_list_entry_ptr(entry: *const ListEntry) -> Self {
-        // Safety: This is safe due to the C representation of the two structs;
-        // Every [`ShellFileInfo`] starts with a [`ListEntry`] so a pointer to [`ListEntry`]
-        // is a pointer to [`ShellFileInfo`] as well.
-        unsafe { *entry.cast::<ShellFileInfo>() }
-    }
-}
-
-/// TODO
-#[repr(C)]
-#[derive(Debug, Clone, Copy)]
-pub struct ListEntry {
-    flink: *mut ListEntry,
-    blink: *mut ListEntry,
-}
-
-/// TODO
-#[derive(Debug)]
-pub struct ShellFileIter {
-    current_node: *const ListEntry,
-}
-
-impl ShellFileIter {
-    fn from_file_info_ptr(ptr: *const ShellFileInfo) -> Self {
-        // Safety: This is safe, as each [`ShellFileInfo`] begins
-        // with a [`ListEntry`] and are #[repr(C)] structs
-        Self {
-            current_node: ptr.cast::<ListEntry>(),
-        }
-    }
-}
-
-impl Iterator for ShellFileIter {
-    type Item = ShellFileInfo;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // Safety: This is safe as we're dereferencing a pointer that we've already null-checked
-        unsafe {
-            if (*self.current_node).flink.is_null() {
-                None
-            } else {
-                let ret = ShellFileInfo::from_list_entry_ptr((*self.current_node).flink);
-                self.current_node = (*self.current_node).flink;
-                Some(ret)
-            }
-        }
     }
 }
